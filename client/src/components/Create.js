13966ddc0131ac34cb7b7a463793cddbeb26d931
Daniel Mattox <dmattox10@gmail.com>
2019-06-24 15:31:22 -0400

    Auto stash before revert of "Players can join and see one another"

--- client/src/components/Create.js
+++ client/src/components/Create.js
@@ -91,7 +91,7 @@ class Create extends Component {
 
     makeid(length) {
         var result           = ''
-        var characters       = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'
+        var characters       = 'ABCDEFGHIJKLMNPQRSTUVWXYZ123456789'
         var charactersLength = characters.length
         for ( var i = 0; i < length; i++ ) {
            result += characters.charAt(Math.floor(Math.random() * charactersLength))
